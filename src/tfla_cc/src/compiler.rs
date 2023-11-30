8b9d10916eec03ff0b0771f001caa7d650e2209d
LucasOliveiraaa <lucas.barros1804sec@gmail.com>
2023-11-29 22:59:02 -0300

    TFLA CC 0.2.3
    Added: TFLAC Analyser, TFLA CC CLI, TFLA CC README updated with commands

--- src/tfla_cc/src/compiler.rs
+++ src/tfla_cc/src/compiler.rs
@@ -14,7 +14,7 @@ impl<'a> Searcher<'a> {
     }
 
     pub fn mount(&self) -> String {
-        let res = format!("se: {} r({})", &self.name[..], &self.regex[..]);
+        let res = format!("se {} {}", &self.name[..], &self.regex[..]);
         res
     }
 
@@ -30,46 +30,71 @@ impl<'a> Searcher<'a> {
     }
 }
 
-pub struct Assembler<'a> {
-    name: &'a str,
-    arbitrary: Vec<&'a str>,
+#[derive(Debug, Clone)]
+pub struct Assembler {
+    name: String,
+    arbitrary: Vec<String>,
+
+    line: usize,
 }
-impl<'a> Assembler<'a> {
-    pub fn new(name: &'a str, arbitrary: Vec<&'a str>) -> Self {
-        Assembler { name, arbitrary }
+impl Assembler {
+    pub fn new(name: String, arbitrary: Vec<String>) -> Self {
+        Assembler { name, arbitrary, line: 0 }
+    }
+
+    pub fn from(text: String) -> Self {
+        let parts: Vec<String> = text.split(" ").map(|a| a.to_string()).collect();
+        let line: usize = parts[0].clone().to_string().parse::<usize>().unwrap();
+        let name: String = parts[2].clone();
+        let arbitrary: Vec<String> = parts[3..].to_vec();
+
+        Assembler { name: String::from(name), arbitrary, line }
     }
 
     pub fn mount(&self) -> String {
-        let mut res = format!("as: {}", &self.name[..]);
+        let mut res = format!("as {}", &self.name[..]);
 
         for a in &self.arbitrary {
             let last = a.len() - 1;
 
             if a.starts_with("<") && a.len() > 2 {
-                if &a[1..last] == self.name {
-                    println!(
-                        "\n>> Impossible Case! The assembler <{}> have a reference to itself!\n",
-                        self.name
-                    );
-
-                    exit(1);
-                }
-
-                res = format!("{} as({})", res, &a[1..last]);
+                res = format!("{} as-{}", res, &a[1..last]);
             } else if a.starts_with("[") && a.len() > 2 {
-                res = format!("{} se({})", res, &a[1..last]);
+                res = format!("{} se-{}", res, &a[1..last]);
             } else if a.starts_with(":") && a.len() > 2 {
-                res = format!("{} sy({})", res, &a[1..last]);
+                res = format!("{} sy-{}", res, &a[1..last]);
             } else {
-                res = format!("{} l({})", res, a);
+                res = format!("{} li-{}", res, a);
             }
         }
 
         res
     }
 
-    pub fn add_arbitrary(&mut self, arbitrary: &'a str) {
-        self.arbitrary.push(arbitrary);
+    pub fn add_arbitrary(&mut self, arbitrary: &str) {
+        self.arbitrary.push(arbitrary.to_string());
+    }
+
+    pub fn have_arbitrary(&self, arbitrary: &str) -> bool {
+        self.arbitrary.contains(&arbitrary.to_string())
+    }
+
+    pub fn arbitrary(&self) -> Vec<String> {
+        self.arbitrary.clone()
+    }
+
+    pub fn name(&self) -> String {
+        self.name.clone()
+    }
+
+    pub fn contain(assemblers: &Vec<Assembler>, value: &str) -> bool {
+        for a in assemblers {
+            if a.name() == value {
+                return true
+            }
+        }
+
+        false
     }
 }
 
@@ -83,7 +108,7 @@ impl<'a> Symbol<'a> {
     }
 
     pub fn mount(&self) -> String {
-        let res = format!("sy: {} r({})", &self.name[..], &self.arbitrary[..]);
+        let res = format!("sy {} {}", &self.name[..], &self.arbitrary[..]);
 
         res
     }
@@ -100,7 +125,10 @@ pub struct TflaCC<'a> {
 
 impl<'a> TflaCC<'a> {
     pub fn new(code: &'a str, searchers: Vec<(&'a str, &'a str)>) -> Self {
-        TflaCC { code, searchers }
+        TflaCC {
+            code,
+            searchers,
+        }
     }
 
     pub fn tokenize(&self) -> Vec<Token<'a>> {
@@ -122,7 +150,7 @@ impl<'a> TflaCC<'a> {
         active: &String,
         value: &'a str,
         se: &mut Searcher<'a>,
-        as_: &mut Assembler<'a>,
+        as_: &mut Assembler,
         sy: &mut Symbol<'a>,
     ) {
         match &active[..] {
@@ -139,29 +167,40 @@ impl<'a> TflaCC<'a> {
         se: &mut Searcher,
         as_: &mut Assembler,
         sy: &mut Symbol,
-    ) {
+        line: usize,
+    ) -> String {
+        let res;
         if active == "searcher" {
-            println!("{}", se.mount())
+            res = format!("\n{} {}", line, se.mount()).to_string();
         } else if active == "assembler" {
-            println!("{}", as_.mount())
+            res = format!("\n{} {}", line, as_.mount()).to_string();
         } else if active == "symbol" {
-            println!("{}", sy.mount())
+            res = format!("\n{} {}", line, sy.mount()).to_string();
         } else {
-            ()
+            return String::new();
         }
 
         *active = "".to_string();
+        res
     }
 
-    pub fn analyze(&self) {
+    fn parse(&self) -> String {
+        let mut res = String::from(r"0 sy nwl (\r)?\n
+0 sy eof \z
+0 sy eol $
+0 sy tab \t 
+0 sy noh \0
+0 sy num \d+");
+
         let tokens = self.tokenize();
 
         let mut token_type: String = String::from("");
         let mut tk_num: i16 = 0;
         let mut in_comment: bool = false;
+        let mut def_line: usize = 0;
 
         let mut searcher: Searcher = Searcher::new("", "");
-        let mut assembler: Assembler = Assembler::new("", vec![]);
+        let mut assembler: Assembler = Assembler::new("".to_string(), vec![]);
         let mut symbol: Symbol = Symbol::new("", "");
 
         for Token {
@@ -172,6 +211,7 @@ impl<'a> TflaCC<'a> {
             ..
         } in &tokens
         {
+            
             tk_num += 1;
             if ty == &"NEW_LINE" {
                 tk_num = 0;
@@ -183,37 +223,43 @@ impl<'a> TflaCC<'a> {
                         continue;
                     }
                     "searcher" => {
-                        self.mount_this(
+                        res += &self.mount_this(
                             &mut token_type,
                             &mut searcher,
                             &mut assembler,
                             &mut symbol,
+                            def_line,
                         );
                         let last = content.len() - 1;
                         token_type = "searcher".to_string();
                         searcher = Searcher::new(&content[1..last], "");
+                        def_line = *line;
                     }
                     "assembler" => {
-                        self.mount_this(
+                        res += &self.mount_this(
                             &mut token_type,
                             &mut searcher,
                             &mut assembler,
                             &mut symbol,
+                            def_line,
                         );
                         let last = content.len() - 1;
                         token_type = "assembler".to_string();
-                        assembler = Assembler::new(&content[1..last], vec![]);
+                        assembler = Assembler::new(content[1..last].to_string(), vec![]);
+                        def_line = *line;
                     }
                     "symbol" => {
-                        self.mount_this(
+                        res += &self.mount_this(
                             &mut token_type,
                             &mut searcher,
                             &mut assembler,
                             &mut symbol,
+                            def_line,
                         );
                         let last = content.len() - 1;
                         token_type = "symbol".to_string();
                         symbol = Symbol::new(&content[1..last], "");
+                        def_line = *line;
                     }
                     "colon" | "pipe" => {
                         if token_type != "assembler" {
@@ -240,6 +286,136 @@ impl<'a> TflaCC<'a> {
                 );
             }
         }
-        self.mount_this(&mut token_type, &mut searcher, &mut assembler, &mut symbol);
+        res += &self.mount_this(
+            &mut token_type,
+            &mut searcher,
+            &mut assembler,
+            &mut symbol,
+            def_line,
+        );
+
+
+        res.trim().to_string()
+    }
+
+    fn get_assembler(&self, name: &str, assemblers: &Vec<Assembler>) -> Vec<Assembler> {
+        let mut res: Vec<Assembler> = vec![];
+
+        for a in assemblers {
+            if a.name() == name {
+                res.push(a.clone());
+            }
+        }
+
+        res
+    }
+
+    fn found_circular_exp(&self, name: String, assembler: &Assembler, assemblers: &Vec<Assembler>) {
+        if assembler.have_arbitrary(&format!("as-{}", name)[..]) {
+            if assembler.name() == name {
+                println!("\nImpossible Case! The Assembler <{}> have a expansion to itself! |{}|", name, assembler.line);
+                exit(0);
+            }else {
+                println!("\nImpossible Case! The Assembler <{}> is part of an circular expansion! |{}|", name, assembler.line);
+                exit(0);
+            }
+        }
+
+        let arbitraries = assembler.arbitrary();
+
+        for arbitrary in arbitraries {
+            if arbitrary.starts_with("as-") {
+                let definitions = self.get_assembler(&arbitrary[3..], assemblers);
+
+                for def in definitions {
+                    self.found_circular_exp(name.clone(), &def, assemblers);
+                }
+            }
+        }
+    }
+
+    fn contain(&self, assemblers: &Vec<(String, usize)>, name: &str) -> bool {
+        for (a, _) in assemblers {
+            if a == name {
+                return true
+            }
+        }
+
+        false
+    }
+
+    pub fn digest(&self) -> String {
+        let code = self.parse();
+
+        let mut lines: Vec<String> = code.split("\n").map(|a| a.to_string()).collect();
+        for line in &mut lines {
+            let parts: Vec<&str> = line.split(" ").collect();
+
+            *line = parts[1..].join(" ").to_string();
+        }
+
+        lines.join("\n")
+    }
+
+    pub fn analyse(&self) -> String {
+        let code = self.parse();
+
+        let mut symbols: Vec<(String, usize)> = vec![];
+        let mut searchers: Vec<(String, usize)> = vec![];
+        let mut assemblers: Vec<Assembler> = vec![];
+
+        let mut lines: Vec<String> = code.split("\n").map(|a| a.to_string()).collect();
+
+        for line in &mut lines {
+            let parts: Vec<String> = line.split(" ").map(|a| a.to_string()).collect();
+            let num_line = parts[0].parse::<usize>().unwrap();
+            let prefix = &parts[1];
+            let name = &parts[2];
+
+            if prefix == "sy" {
+                symbols.push((name.clone(), num_line));
+            } else if prefix == "se" {
+                searchers.push((name.clone(), num_line));
+            } else if prefix == "as" {
+                assemblers.push(Assembler::from(line.clone()));
+            }
+
+            *line = parts[1..].join(" ");
+        }
+
+        for assembler in &assemblers.clone() {
+            let name = assembler.name();
+
+            let arbitraries = assembler.arbitrary();
+
+            for arbitrary in &arbitraries {
+                let prefix = &arbitrary[..3];
+                let value = &arbitrary[3..];
+
+                if prefix == "sy-" && !self.contain(&symbols, &value) {
+                    println!("\nERROR: The Assembler <{}> have an expansion to Symbol :{}:, but it's don't exists. |{}|",
+                        name, 
+                        value, assembler.line);
+                    exit(0); 
+                }else if prefix == "se-" && !self.contain(&searchers, &value) {
+                    println!("\nERROR: The Assembler <{}> have an expansion to Searcher [{}], but it's don't exists. |{}|",
+                        name, 
+                        value, assembler.line);
+                    exit(0); 
+                }else if prefix == "as-" {
+                    if !Assembler::contain(&assemblers, &value) {
+                        println!("\nERROR: The Assembler <{}> have an expansion to Assembler <{}>, but it's don't exists. |{}|",
+                            name, 
+                            value, assembler.line);
+
+                        exit(0);
+                    }
+
+                    self.found_circular_exp(name.clone(), &assembler, &assemblers);
+                }
+            }
+        }
+
+        lines.join("\n")
     }
 }
--- src/tfla_cc/src/main.rs
+++ src/tfla_cc/src/main.rs
@@ -1,17 +1,110 @@
+use flate2::write::GzEncoder;
+use flate2::Compression;
 use std::env;
 use std::fs;
+use std::fs::File;
+use std::io::{self, Write};
+use std::process::exit;
+
+fn compress(data: &[u8]) -> io::Result<Vec<u8>> {
+    let mut compressed_data = Vec::new();
+
+    let mut encoder = GzEncoder::new(Vec::new(), Compression::default());
+    encoder.write_all(data)?;
+    compressed_data.extend_from_slice(&encoder.finish()?);
+
+    Ok(compressed_data)
+}
 
 mod compiler;
 use compiler::{Searcher, TflaCC};
+
+fn show_help() {
+    println!(
+        "
+TFLA CC - TFLA Config \"Compiler\"
+
+tfla-cc <command> [arguments]
+
+General Commands:
+    --help | -h 
+        Arguments: [command]
+        Show this message or show the help of some command.
+    --digest | -d
+        Arguments: <path> [-a] [-w|-W] [output]
+        Compile a TFLAC source and show the result.
+    --analyse | -a 
+        Arguments: <path>
+        Just analyse the source code to syntax and impossible cases.
+    --compile | -c 
+        Arguments: <path>
+        Compile the passed source code with type .tflac to an .exaust.tfla file with same name.
+        aka tfla-cc -d <path> -a -W"
+    );
+}
+
+fn write_file(file: String, content: String) -> std::io::Result<()> {
+    let mut f = File::create(file.clone())?;
+
+    let byt = content.as_bytes();
+
+    f.write_all(&compress(byt)?)?;
+
+    println!("Exaust in {}", file);
+
+    Ok(())
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
-        panic!("ERROR: Any file .tflac was passed!");
+        show_help();
+
+        exit(0);
     }
 
-    let content = fs::read_to_string(&args[1]).expect("Can't open the file.");
+    let co = &args[1];
+
+    if co == "-h" || co == "--help" {
+        show_help();
+
+        exit(0);
+    }
+
+    let mut analyse = false;
+    let mut write_same = false;
+    let mut write_in = false;
+    let mut output = String::from("");
+    let mut input = String::from("");
+
+    #[allow(unused_assignments)]
+    for arg in &args[2..] {
+        if co == "-d" || co == "--digest" {
+            if arg == "-a" {
+                analyse = true;
+            } else if arg == "-w" {
+                write_in = true;
+            } else if arg == "-W" {
+                write_same = true;
+            } else {
+                if input == "".to_string() {
+                    input = arg.to_string();
+                } else {
+                    output = arg.to_string();
+                }
+            }
+        } else {
+            input = arg.to_string();
+        }
+    }
+    #[warn(unused_assignments)]
+    if input == "" {
+        println!("Pass an input file.");
+        exit(0);
+    }
+
+    let content = fs::read_to_string(input.clone()).expect("Can't open the input file");
 
     let mut c_searchers: Vec<(&str, &str)> = vec![];
     c_searchers.push(Searcher::new("comment", r"^\-\-").transform());
@@ -20,12 +113,54 @@ fn main() {
     c_searchers.push(Searcher::new("searcher", r"^\[(\w|_)+\]").transform());
     c_searchers.push(Searcher::new("assembler", r"^<(\w|_)+>").transform());
     c_searchers.push(Searcher::new("symbol", r"^\:(\w|_)+\:").transform());
-    c_searchers.push(Searcher::new("colon", r"^\:").transform());
-    c_searchers.push(Searcher::new("colon", r"^\:r").transform());
+    c_searchers.push(Searcher::new("colon", r"^(:|:r|::=)").transform());
     c_searchers.push(Searcher::new("pipe", r"^\|").transform());
     c_searchers.push(Searcher::new("entity", r"^[^\s]*").transform());
 
     let cc: TflaCC = TflaCC::new(&content[..], c_searchers);
 
-    cc.analyze();
+    if co == "-d" || co == "--digest" {
+        let res: String;
+
+        if analyse {
+            res = cc.analyse();
+        } else {
+            res = cc.digest();
+        }
+
+        if write_in {
+            if output == "" {
+                println!("Pass an output file.");
+                exit(0);
+            } else {
+                let _ = write_file(output, res);
+            }
+        } else if write_same {
+            let mut parts: Vec<String> = input.split(".").map(|a| a.to_string()).collect();
+
+            let last = parts.len() - 1;
+            parts[last] = "exaust.tfla".to_string();
+
+            let _ = write_file(parts.join("."), res);
+        } else {
+            println!("{}", res);
+        }
+    }
+
+    if co == "-a" || co == "--analyse" {
+        cc.analyse();
+
+        println!("\nEnd with 0 Error and 0 Warns");
+    }
+
+    if co == "-c" || co == "--compile" {
+        let res = cc.analyse();
+
+        let mut parts: Vec<String> = input.split(".").map(|a| a.to_string()).collect();
+
+        let last = parts.len() - 1;
+        parts[last] = "exaust.tfla".to_string();
+
+        let _ = write_file(parts.join("."), res);
+    }
 }
